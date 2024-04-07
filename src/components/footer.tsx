2c23e8576f70951f61ccffee9a9daacc84729682
arieltroian <arieltroian@outlook.com>
2024-04-07 15:17:35 -0300

    feat: add Footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import { Card, CardContent } from "./ui/card";
+
+const Footer = () => {
+  return (
+    <Card>
+      <CardContent className="flex items-center justify-center pb-0 p-5">
+        <h3 className="font-extralight text-lg">Feito com 💚</h3>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Footer;
