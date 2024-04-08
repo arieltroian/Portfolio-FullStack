fe7f412f7db3f4794c5e596fd3a7ddfed0609c5c
arieltroian <arieltroian@outlook.com>
2024-04-08 16:25:30 -0300

    feat: add initial Projects List Component

---
+++ src/app/projects/projectList.tsx
@@ -0,0 +1,48 @@
+interface Projects {
+  id: number;
+  title: string;
+  subtile: string;
+  description: string;
+  technologyTags: string[];
+  projectPreviewLink: string;
+  githubRepositoryLink: string;
+  previewImageUrl: string;
+  responsiveImageUrl: string;
+}
+
+const ProjectsList: Projects[] = [
+  {
+    id: 1,
+    title: "Projeto 1",
+    subtile: "Lorem ipsum dolor sit amet. Est voluptatem ipsam",
+    description: "",
+    technologyTags: [""],
+    projectPreviewLink: "",
+    githubRepositoryLink: "",
+    previewImageUrl: "",
+    responsiveImageUrl: "",
+  },
+  {
+    id: 2,
+    title: "Projeto 2",
+    subtile: "Lorem ipsum dolor sit amet. Est voluptatem ipsam",
+    description: "",
+    technologyTags: [""],
+    projectPreviewLink: "",
+    githubRepositoryLink: "",
+    previewImageUrl: "",
+    responsiveImageUrl: "",
+  },
+  {
+    id: 3,
+    title: "Projeto 3",
+    subtile: "Lorem ipsum dolor sit amet. Est voluptatem ipsam",
+    description: "",
+    technologyTags: [""],
+    projectPreviewLink: "",
+    githubRepositoryLink: "",
+    previewImageUrl: "",
+    responsiveImageUrl: "",
+  },
+];
+export default ProjectsList;
