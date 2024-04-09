be2e350ab7f718d1191afdee828bb36754466b65
arieltroian <arieltroian@outlook.com>
2024-04-09 14:20:15 -0300

    feat: add projects infos to Project List Component

--- src/app/projects/projectList.tsx
+++ src/app/projects/projectList.tsx
@@ -15,38 +15,69 @@ const ProjectsList: Projects[] = [
   {
     id: 1,
     projectNumber: "01.",
-    title: "Projeto 1",
-    subtitle: "Lorem ipsum dolor sit amet. Est voluptatem ipsam",
-    description: "",
-    technologyTags: [""],
-    projectPreviewLink: "",
-    githubRepositoryLink: "",
-    previewImageUrl: "",
+    title: "Cãotinho PetCare",
+    subtitle: "Agendamento de serviços",
+    description:
+      "O Cãotinho Pet Care é uma aplicação fullstack que simplifica o agendamento de serviços para animais de estimação, visando oferecer uma experiência única e conveniente para tutores. Seus principais objetivos são facilitar agendamentos, centralizar informações e garantir autenticação segura. Entre suas funcionalidades estão agendamento e cancelamento de serviços, com autenticação via conta do Google para segurança dos dados. Utiliza tecnologias como TypeScript, React.js, Next.js, Node.js, Prisma, Supabase, Google Auth, Tailwind CSS e Shadcn/ui para um desenvolvimento robusto, rápido e seguro.",
+    technologyTags: [
+      "TypeScript",
+      "React",
+      "Next.js",
+      "Tailwind CSS",
+      "Shadcn/UI",
+      "Node",
+      "Prisma",
+      "Supabase",
+    ],
+    projectPreviewLink: "https://caotinho-pet-care.vercel.app/",
+    githubRepositoryLink: "https://github.com/arieltroian/CaotinhoPetCare",
+    previewImageUrl:
+      "https://i.ibb.co/Ws8gnNM/preview-image-caotinho-petcare.png",
     responsiveImageUrl: "",
   },
   {
     id: 2,
     projectNumber: "02.",
-    title: "Projeto 2",
-    subtitle: "Lorem ipsum dolor sit amet. Est voluptatem ipsam",
-    description: "",
-    technologyTags: [""],
+    title: "API Agenda de Contatos ",
+    subtitle: "API CRUD",
+    description:
+      "Back-end API CRUD (Create, Read, Update, Delete) para realizar operações em uma agenda de contatos.",
+    technologyTags: ["TypeScript", "Node", "Prisma", "Fastify"],
     projectPreviewLink: "",
-    githubRepositoryLink: "",
-    previewImageUrl: "",
+    githubRepositoryLink:
+      "https://github.com/arieltroian/Agenda-de-Contatos-CRUD-API",
+    previewImageUrl: "https://i.ibb.co/3B19KmJ/preview-image-contacts-api.png",
     responsiveImageUrl: "",
   },
   {
     id: 3,
     projectNumber: "03.",
-    title: "Projeto 3",
-    subtitle: "Lorem ipsum dolor sit amet. Est voluptatem ipsam",
-    description: "",
-    technologyTags: [""],
-    projectPreviewLink: "",
-    githubRepositoryLink: "",
-    previewImageUrl: "",
+    title: "Calculadora",
+    subtitle: "Projeto de Calculadora",
+    description:
+      "Estudo feito com o intuito de fixar conhecimentos e desenvolver habilidades de lógica com JavaScript",
+    technologyTags: ["HTML", "CSS", "JavaScript"],
+    projectPreviewLink: "https://arieltroian-projeto-calculadora.netlify.app",
+    githubRepositoryLink:
+      "https://github.com/arieltroian/Projeto-Calculadora?tab=readme-ov-file",
+    previewImageUrl: "https://i.ibb.co/0mfm5C7/preview-image-calculdora.png",
+    responsiveImageUrl: "",
+  },
+  {
+    id: 4,
+    projectNumber: "04.",
+    title: "Relógio Digital",
+    subtitle: "Projeto de Relógio Digital",
+    description:
+      "Este é um projeto simples, que teve como objetivo principal a prática de JavaScript. Não tive muitas dificuldas na realização.",
+    technologyTags: ["HTML", "CSS", "JavaScript"],
+    projectPreviewLink: "https://arieltroian.github.io/Relogio-Digital/",
+    githubRepositoryLink:
+      "https://github.com/arieltroian/Relogio-Digital?tab=readme-ov-file",
+    previewImageUrl:
+      "https://i.ibb.co/jVJbdV4/preview-image-relogio-digital.png",
     responsiveImageUrl: "",
   },
 ];
+
 export default ProjectsList;
