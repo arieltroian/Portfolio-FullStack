b1015534eb32fcc687b8055e2af4edb8aaebf018
arieltroian <arieltroian@outlook.com>
2024-04-09 08:51:48 -0300

    feat: add technology badges to Home Page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -9,10 +9,11 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import ExternalLink from "@/components/externalLink";
+import { Badge } from "@/components/ui/badge";
 
 const HomePage = () => {
   return (
-    <section className="flex items-center justify-center mt-[-6rem] lg:mt-[-4rem]">
+    <section className="flex items-center justify-center mt-[-2rem]">
       <div className="flex flex-wrap lg:flex-nowrap gap-8 overflow-hidden w-full">
         <div className="my-1 w-full sm:px-1 lg:w-4/12">
           <div className="px-6 flex flex-col items-center justify-center mt-3">
@@ -50,30 +51,43 @@ const HomePage = () => {
           </div>
         </div>
 
-        <Card className="p-2 lg:p-4 my-1 w-full sm:px-1 lg:w-8/12">
+        <Card className="p-2 w-full sm:px-1 lg:w-8/12">
           <CardContent className="flex flex-col items-center justify-center">
             <Accordion type="single" collapsible className="w-full">
               <AccordionItem value="item-1">
-                <AccordionTrigger>Linguagens favoritas</AccordionTrigger>
-                <AccordionContent>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Autem, placeat?
+                <AccordionTrigger>Linguagens</AccordionTrigger>
+                <AccordionContent className="flex flex-wrap gap-2">
+                  <Badge>JavaScript</Badge>
+                  <Badge>TypeSecript</Badge>
+                  <Badge>Python</Badge>
                 </AccordionContent>
               </AccordionItem>
 
               <AccordionItem value="item-2">
-                <AccordionTrigger>Frameworks e Libs</AccordionTrigger>
-                <AccordionContent>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Autem, placeat?
+                <AccordionTrigger>Front-End</AccordionTrigger>
+                <AccordionContent className="flex flex-wrap gap-2">
+                  <Badge>HTML</Badge>
+                  <Badge>CSS</Badge>
+                  <Badge>React</Badge>
+                  <Badge>Next.js</Badge>
+                  <Badge>Tailwind CSS</Badge>
                 </AccordionContent>
               </AccordionItem>
 
               <AccordionItem value="item-3">
-                <AccordionTrigger>ORM e Banco de dados</AccordionTrigger>
-                <AccordionContent>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Autem, placeat?
+                <AccordionTrigger>Back-End</AccordionTrigger>
+                <AccordionContent className="flex flex-wrap gap-2">
+                  <Badge>Node</Badge>
+                  <Badge>Fastify</Badge>
+                </AccordionContent>
+              </AccordionItem>
+
+              <AccordionItem value="item-4">
+                <AccordionTrigger>Banco de dados</AccordionTrigger>
+                <AccordionContent className="flex flex-wrap gap-2">
+                  <Badge>MongoDB</Badge>
+                  <Badge>Supabase</Badge>
+                  <Badge>Prisma</Badge>
                 </AccordionContent>
               </AccordionItem>
             </Accordion>
