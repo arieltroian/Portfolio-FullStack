3bc766d4a23406a6430232033afa0bd26212c6d1
arieltroian <arieltroian@outlook.com>
2024-04-13 20:23:17 -0300

    feat: add PreLoad Component to Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,38 @@
+"use client";
+
 import HomePage from "./home/page";
 import ProjectsPage from "./projects/page";
 import ContactPage from "./contact/page";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import { useState, useEffect } from "react";
+import PreLoad from "@/components/preload";
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
   return (
     <main>
-      <div className="px-4 md:px-6 lg:px-10">
-        <Header />
-      </div>
-      <div className="px-4 md:px-6 lg:px-20 xl:px-28">
-        <HomePage />
-        <ProjectsPage />
-        <ContactPage />
-      </div>
-      <Footer />
+      {loading ? (
+        <PreLoad />
+      ) : (
+        <div>
+          <div className="px-4 md:px-6 lg:px-10">
+            <Header />
+          </div>
+          <div className="px-4 md:px-6 lg:px-20 xl:px-28">
+            <HomePage />
+            <ProjectsPage />
+            <ContactPage />
+          </div>
+          <Footer />
+        </div>
+      )}
     </main>
   );
 }
