c65db056479a1cb91a8a9750a3b72463a516c0a3
arieltroian <arieltroian@outlook.com>
2024-05-10 13:39:48 -0300

    feat: remove pre-loading function on site entry"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,34 +5,21 @@ import ProjectsPage from "./projects/page";
 import ContactPage from "./contact/page";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
-import { useState, useEffect } from "react";
-import PreLoad from "@/components/preload";
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 1500);
-  }, []);
   return (
     <main>
-      {loading ? (
-        <PreLoad />
-      ) : (
-        <div>
-          <div className="px-4 md:px-6 lg:px-10">
-            <Header />
-          </div>
-          <div className="px-4 md:px-6 lg:px-20 xl:px-28">
-            <HomePage />
-            <ProjectsPage />
-            <ContactPage />
-          </div>
-          <Footer />
+      <div>
+        <div className="px-4 md:px-6 lg:px-10">
+          <Header />
         </div>
-      )}
+        <div className="px-4 md:px-6 lg:px-20 xl:px-28">
+          <HomePage />
+          <ProjectsPage />
+          <ContactPage />
+        </div>
+        <Footer />
+      </div>
     </main>
   );
 }
